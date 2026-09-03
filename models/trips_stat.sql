6334615c874dfe4cbd53dccc806b28de9d89d354
LShavzis <lshavzis@gmail.com>
2026-09-04 00:56:51 +0200

    Changing trips stats generation to use trips_prep model instead of raw trips table. Added sum_distance_km metric to trips_stat model.

--- models/trips_stat.sql
+++ models/trips_stat.sql
@@ -1,9 +1,11 @@
-{{ config(materialized='table') }}
+
 
 select
 count(id) as trips
 ,count(distinct(user_id)) as users
-,avg(extract(EPOCH FROM (finished_at - started_at)) / 60) as avg_duration_m 
-,sum(price) as revenue_rub
-,round(count(id) filter (where price = 0):: numeric / count(id) * 100 ,2) as free_trips_pct 
-from dev_487w.scooters_raw.trips t
+,avg(duration_s / 60) as avg_duration_m 
+,sum(price_rub) as revenue_rub
+,round(count(id) filter (where is_free = 1):: numeric / count(id) * 100 ,2) as free_trips_pct
+,sum(distance_m) / 1000 as sum_distance_km 
+from {{ref('trips_prep')}}
+where started_at >= '2023-01-01'
